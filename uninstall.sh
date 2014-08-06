45adcc3c9f460a18eca6c5360a7fde85f1779d5a
Joshua Green <josh@softwareverde.com>
2014-08-06 17:08:47 -0400

    Updated install.sh to detect if bitcoind is already running before stopping it. Invokes update-rc.d to remove autostart.

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,12 @@ fi
 
 DAEMON_USER='bitcoin'
 
-/etc/init.d/bitcoind stop
+/etc/init.d/bitcoind status 1>/dev/null 2>/dev/null
+if [ "$?" -eq 0 ]; then
+    /etc/init.d/bitcoind stop
+fi
+
+update-rc.d -f bitcoind remove
 deluser "${DAEMON_USER}"
 rm -r "/home/${DAEMON_USER}"
 rm /etc/init.d/bitcoind
