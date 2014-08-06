6f2411d0058c02c9e4d85e6a9b6a25876daa577c
Joshua Green <josh@softwareverde.com>
2014-08-06 15:50:44 -0400

    Updated blockchain_progress to only poll the internet once every ~30 seconds.

--- blockchain_progress.sh
+++ blockchain_progress.sh
@@ -11,16 +11,22 @@ if [ ! -f ~/.bitcoin/bitcoin.conf ]; then
 fi
 
 if [ "$*" == "--poll" ]; then
+    i=0
+    web_count=`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`
     while [ 0 -eq 0 ]; do
         local_count=`bitcoin-cli getblockcount 2>&1`
         if [ "$?" -gt 0 ]; then
             echo "ERROR: Can't connect to bitcoind. Is it running?" 1>&2
             exit 1
         fi
-        web_count=`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`
+        if [ $i = 10 ]; then
+            web_count=`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`
+            i=0
+        fi
 
         echo -ne "\r\033[KDownloaded:\t${local_count} of ${web_count}\t\t( $((${local_count} * 100 / ${web_count}))% )"
         sleep 3
+        i=$(($i +1))
     done
 else
     local_count=`bitcoin-cli getblockcount 2>&1`
